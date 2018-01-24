d657862c44d52acce2b487a7f864059375622bab
沈杰 <247570373@qq.com>
2018-01-24 21:58:35 +0800

    颜色SKU

--- Application/Admin/Controller/GoodsController.class.php
+++ Application/Admin/Controller/GoodsController.class.php
@@ -5,15 +5,16 @@ namespace Admin\Controller;
 class GoodsController extends BaseController
 {
     private $status = array(
-        '0' =>'上架',
-        '1' =>'下架',
-        '2' =>'删除'
+        '0' => '上架',
+        '1' => '下架',
+        '2' => '删除'
     );
+
     public function index()
     {
         $model = M('Goods');
-        $list = $model->join('LEFT JOIN l_goods_picture on l_goods.id = l_goods_picture.goods_id')->where(array('l_goods.status'=>array('neq',2),'l_goods_picture.type'=>0))->group('l_goods.id')->field('l_goods.*,l_goods_picture.url')->select();
-        foreach ($list as &$v){
+        $list = $model->join('LEFT JOIN l_goods_picture on l_goods.id = l_goods_picture.goods_id')->where(array('l_goods.status' => array('neq', 2), 'l_goods_picture.type' => 0))->group('l_goods.id')->field('l_goods.*,l_goods_picture.url')->select();
+        foreach ($list as &$v) {
             $v['status'] = $this->status[$v['status']];
         }
         $this->assign('list', $list);
@@ -32,32 +33,35 @@ class GoodsController extends BaseController
             ->join('LEFT JOIN l_goods_picture on l_goods.id = l_goods_picture.goods_id')
             ->where('l_goods.id=' . I('get.id'))->field('l_goods.*,l_goods_picture.url,l_goods_picture.type')->select();
         //规格部分
-        $arr_spe = M('GoodsSpe')->where('goods_id='.I('get.id'))->order(array('value'))->select();
-        foreach ($arr_spe as $v){
-            if($v['type'] == 0){
+        $arr_spe = M('GoodsSpe')->where('goods_id=' . I('get.id'))->order(array('value'))->select();
+        foreach ($arr_spe as $v) {
+            if ($v['type'] == 0) {
                 $spe_value_1[] = $v['value'];
-            }else{
+            } else {
                 $spe_value_2[] = $v['value'];
+                $picture_3[] = $v['url'];
             }
         }
-        $spe_value_1 = implode(',',$spe_value_1);
-        $spe_value_2 = implode(',',$spe_value_2);
-        foreach ($detail as $v){
-            if($v['type']== 0){
+        $spe_value_1 = implode(',', $spe_value_1);
+        $spe_value_2 = implode(',', $spe_value_2);
+        foreach ($detail as $v) {
+            if ($v['type'] == 0) {
                 $picture_0[] = $v['url'];
-            }elseif ($v['type'] == 1){
-                $picture_1[] =$v['url'];
+            } elseif ($v['type'] == 1) {
+                $picture_1[] = $v['url'];
             }
         }
-        $picture_value_0 = implode(',',$picture_0);
-        $picture_value_1 = implode(',',$picture_1);
-        $detail = array_merge($detail[0],array('picture_0'=>$picture_0),array('picture_1'=>$picture_1));
+        $picture_value_0 = implode(',', $picture_0);
+        $picture_value_1 = implode(',', $picture_1);
+        $picture_value_3 = implode(',', $picture_3);
+        $detail = array_merge($detail[0], array('picture_0' => $picture_0), array('picture_1' => $picture_1),array('picture_3'=>$picture_3));
         $detail['picture_value_0'] = $picture_value_0;
         $detail['picture_value_1'] = $picture_value_1;
+        $detail['picture_value_3'] = $picture_value_3;
         $detail['spe_value_1'] = $spe_value_1;
         $detail['spe_value_2'] = $spe_value_2;
         //评价部分
-        $arr_evalute = M('GoodsEvaluate')->where('goods_id='.I('get.id'))->order(array('id'))->select();
+        $arr_evalute = M('GoodsEvaluate')->where('goods_id=' . I('get.id'))->order(array('id'))->select();
         $detail['evalute'] = $arr_evalute;
         $this->assign('detail', $detail);
         $this->display();
@@ -77,50 +81,54 @@ class GoodsController extends BaseController
         $model = M('Goods');
         $result = $model->add($param);
         //图片开始
-        $photo_value1 = explode(',',trim(I('post.photo_value1'),','));
-        foreach ($photo_value1 as $v){
-            $arr_file[] = array('url'=>$v,'type' =>0 ,'goods_id'=>$result);
-            rename('./Public/temp/'.$v,'./Public/images/'.$v);
+        $photo_value1 = explode(',', trim(I('post.photo_value1'), ','));
+        foreach ($photo_value1 as $v) {
+            $arr_file[] = array('url' => $v, 'type' => 0, 'goods_id' => $result);
+            rename('./Public/temp/' . $v, './Public/images/' . $v);
         }
-        $photo_value2 = explode(',',trim(I('post.photo_value2'),','));
-        foreach ($photo_value2 as $v){
-            $arr_file[] = array('url'=>$v,'type' =>1 ,'goods_id'=>$result);
-            rename('./Public/temp/'.$v,'./Public/images/'.$v);
+        $photo_value2 = explode(',', trim(I('post.photo_value2'), ','));
+        foreach ($photo_value2 as $v) {
+            $arr_file[] = array('url' => $v, 'type' => 1, 'goods_id' => $result);
+            rename('./Public/temp/' . $v, './Public/images/' . $v);
         }
         $photo_model = M('GoodsPicture');
         $photo_model->addAll($arr_file);
         //规格开始
-        if(!empty(I('post.spe_value_1'))){
-            $spe_value_1 = explode(',',I('post.spe_value_1'));
-            foreach ($spe_value_1 as $v){
+        if (!empty(I('post.spe_value_1'))) {
+            $spe_value_1 = explode(',', I('post.spe_value_1'));
+            foreach ($spe_value_1 as $v) {
                 $arr_spe[] = array(
-                    'type'=>0,
-                    'value'=>$v,
-                    'goods_id'=>$result
+                    'type' => 0,
+                    'value' => $v,
+                    'url' => '',
+                    'goods_id' => $result
                 );
             }
         }
-        if(!empty(I('post.spe_value_2'))){
-            $spe_value_1 = explode(',',I('post.spe_value_2'));
-            foreach ($spe_value_1 as $v){
+        if (!empty(I('post.spe_value_2'))) {
+            $spe_value_1 = explode(',', I('post.spe_value_2'));
+            $photo_value3 = explode(',', trim(I('post.photo_value3'), ','));
+            for ($i = 0; $i < count($spe_value_1); $i++) {
+                rename('./Public/temp/' . $photo_value3[$i], './Public/images/' . $photo_value3[$i]);
                 $arr_spe[] = array(
-                    'type'=>1,
-                    'value'=>$v,
-                    'goods_id'=>$result
+                    'type' => 1,
+                    'value' => $spe_value_1[$i],
+                    'url' => $photo_value3[$i],
+                    'goods_id' => $result
                 );
             }
         }
-        if(!empty($arr_spe)){
+        if (!empty($arr_spe)) {
             M('GoodsSpe')->addAll($arr_spe);
         }
         //评价开始
         $evaluate_user = I('post.evaluate_user');
         $evaluate_value = I('post.evaluate_value');
-        for ($i=0;$i<count($evaluate_user);$i++){
+        for ($i = 0; $i < count($evaluate_user); $i++) {
             $evalute_data[] = array(
-                'user'=>$evaluate_user[$i],
-                'content'=>$evaluate_value[$i],
-                'goods_id'=>$result
+                'user' => $evaluate_user[$i],
+                'content' => $evaluate_value[$i],
+                'goods_id' => $result
             );
         }
         M('GoodsEvaluate')->addAll($evalute_data);
@@ -146,74 +154,85 @@ class GoodsController extends BaseController
         $result = $model->where('id=' . I('post.id'))->save($param);
         //图片处理部分
         $photo_model = M('GoodsPicture');
-        if(!empty(I('post.photo_value1'))){
+        if (!empty(I('post.photo_value1'))) {
             //先删除
-            $photo_model->where(array('goods_id'=>I('post.id'),'type'=>0))->delete();
-            $picture_value_0 = explode(',',I('post.picture_value_0'));
-            foreach ($picture_value_0 as $v){
-                unlink('./Public/images/'.$v);
+            $photo_model->where(array('goods_id' => I('post.id'), 'type' => 0))->delete();
+            $picture_value_0 = explode(',', I('post.picture_value_0'));
+            foreach ($picture_value_0 as $v) {
+                unlink('./Public/images/' . $v);
             }
             //后添加
-            $photo_value1 = explode(',',trim(I('post.photo_value1'),','));
-            foreach ($photo_value1 as $v){
-                $arr_file[] = array('url'=>$v,'type' =>0 ,'goods_id'=>I('post.id'));
-                rename('./Public/temp/'.$v,'./Public/images/'.$v);
+            $photo_value1 = explode(',', trim(I('post.photo_value1'), ','));
+            foreach ($photo_value1 as $v) {
+                $arr_file[] = array('url' => $v, 'type' => 0, 'goods_id' => I('post.id'));
+                rename('./Public/temp/' . $v, './Public/images/' . $v);
             }
         }
-        if(!empty(I('post.photo_value2'))){
+        if (!empty(I('post.photo_value2'))) {
             //先删除
-            $photo_model->where(array('goods_id'=>I('post.id'),'type'=>1))->delete();
-            $picture_value_1 = explode(',',I('post.picture_value_1'));
-            foreach ($picture_value_1 as $v){
-                unlink('./Public/images/'.$v);
+            $photo_model->where(array('goods_id' => I('post.id'), 'type' => 1))->delete();
+            $picture_value_1 = explode(',', I('post.picture_value_1'));
+            foreach ($picture_value_1 as $v) {
+                unlink('./Public/images/' . $v);
             }
             //后添加
-            $photo_value2 = explode(',',trim(I('post.photo_value2'),','));
-            foreach ($photo_value2 as $v){
-                $arr_file[] = array('url'=>$v,'type' =>1 ,'goods_id'=>I('post.id'));
-                rename('./Public/temp/'.$v,'./Public/images/'.$v);
+            $photo_value2 = explode(',', trim(I('post.photo_value2'), ','));
+            foreach ($photo_value2 as $v) {
+                $arr_file[] = array('url' => $v, 'type' => 1, 'goods_id' => I('post.id'));
+                rename('./Public/temp/' . $v, './Public/images/' . $v);
             }
         }
-        if(!empty($arr_file)){
+        if (!empty($arr_file)) {
             $photo_model->addAll($arr_file);
         }
 
         //规格开始
         $spe_model = M('GoodsSpe');
-        $spe_model->where(array('goods_id'=>I('post.id')))->delete();
-        if(!empty(I('post.spe_value_1'))){
-            $spe_value_1 = explode(',',I('post.spe_value_1'));
-            foreach ($spe_value_1 as $v){
+        //先删除
+        $data = $spe_model->where(array('goods_id' => I('post.id')))->select();
+        foreach ($data as $v){
+            if(!empty($v['url'])){
+                unlink('./Public/images/' . $v);
+            }
+        }
+        $spe_model->where(array('goods_id' => I('post.id')))->delete();
+        if (!empty(I('post.spe_value_1'))) {
+            $spe_value_1 = explode(',', I('post.spe_value_1'));
+            foreach ($spe_value_1 as $v) {
                 $arr_spe[] = array(
-                    'type'=>0,
-                    'value'=>$v,
-                    'goods_id'=>I('post.id')
+                    'type' => 0,
+                    'value' => $v,
+                    'url' => '',
+                    'goods_id' => I('post.id')
                 );
             }
         }
-        if(!empty(I('post.spe_value_2'))){
-            $spe_value_1 = explode(',',I('post.spe_value_2'));
-            foreach ($spe_value_1 as $v){
+        if (!empty(I('post.spe_value_2'))) {
+            $spe_value_1 = explode(',', I('post.spe_value_2'));
+            $photo_value3 = explode(',', trim(I('post.photo_value3'), ','));
+            for ($i = 0; $i < count($spe_value_1); $i++) {
+                rename('./Public/temp/' . $photo_value3[$i], './Public/images/' . $photo_value3[$i]);
                 $arr_spe[] = array(
-                    'type'=>1,
-                    'value'=>$v,
-                    'goods_id'=>I('post.id')
+                    'type' => 1,
+                    'value' => $spe_value_1[$i],
+                    'url' => $photo_value3[$i],
+                    'goods_id' => I('post.id')
                 );
             }
         }
-        if(!empty($arr_spe)){
+        if (!empty($arr_spe)) {
             $spe_model->addAll($arr_spe);
         }
         //评价开始
         $eva_model = M('GoodsEvaluate');
-        $eva_model->where(array('goods_id'=>I('post.id')))->delete();
+        $eva_model->where(array('goods_id' => I('post.id')))->delete();
         $evaluate_user = I('post.evaluate_user');
         $evaluate_value = I('post.evaluate_value');
-        for ($i=0;$i<count($evaluate_user);$i++){
+        for ($i = 0; $i < count($evaluate_user); $i++) {
             $evalute_data[] = array(
-                'user'=>$evaluate_user[$i],
-                'content'=>$evaluate_value[$i],
-                'goods_id'=>I('post.id')
+                'user' => $evaluate_user[$i],
+                'content' => $evaluate_value[$i],
+                'goods_id' => I('post.id')
             );
         }
         $eva_model->addAll($evalute_data);
@@ -227,17 +246,24 @@ class GoodsController extends BaseController
     public function update_status()
     {
         $model = M('Goods');
-        $result = $model->where('id=' . I('get.id'))->save(array('status'=>I('get.status')));
-        if(I('get.status') == 2){
+        $result = $model->where('id=' . I('get.id'))->save(array('status' => I('get.status')));
+        if (I('get.status') == 2) {
             $photo_model = M('GoodsPicture');
-            $list = $photo_model->where('goods_id='.I('get.id'))->select();
-            foreach ($list as $v){
-                unlink('./Public/images/'.$v['url']);
+            $list = $photo_model->where('goods_id=' . I('get.id'))->select();
+            foreach ($list as $v) {
+                unlink('./Public/images/' . $v['url']);
             }
-            $photo_model->where('goods_id='.I('get.id'))->delete();
-            M('GoodsSpe')->where('goods_id='.I('get.id'))->delete();
+            $photo_model->where('goods_id=' . I('get.id'))->delete();
+            $spe_model = M('GoodsSpe');
+            $data = $spe_model->where(array('goods_id' => I('get.id')))->select();
+            foreach ($data as $v){
+                if(!empty($v['url'])){
+                    unlink('./Public/images/' . $v);
+                }
+            }
+            $spe_model->where('goods_id=' . I('get.id'))->delete();
         }
-        if ($result!==false) {
+        if ($result !== false) {
             $this->success('操作成功', U('index'));
         } else {
             $this->error('操作失败');
@@ -246,8 +272,8 @@ class GoodsController extends BaseController
 
     public function photo_upload()
     {
-        $file_name = md5(time().mt_rand(1000000,9999999)) . "." . substr(strrchr($_FILES['file']['name'], '.'), 1);
+        $file_name = md5(time() . mt_rand(1000000, 9999999)) . "." . substr(strrchr($_FILES['file']['name'], '.'), 1);
         copy($_FILES["file"]["tmp_name"], './Public/temp/' . $file_name);
-        $this->ajaxReturn(array('src'=>$file_name));
+        $this->ajaxReturn(array('src' => $file_name));
     }
 }
